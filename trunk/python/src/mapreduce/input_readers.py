83e71e63f706e60ee345f06e059bdb8816fe9e19
Tony Liu <tony@khanacademy.org>
2013-12-06 14:56:59 -0800

    Add filtering into new key splitting logic
    Test Plan:
    Run MR jobs locally and on a znd

--- trunk/python/src/mapreduce/input_readers.py
+++ trunk/python/src/mapreduce/input_readers.py
@@ -519,6 +519,7 @@ class AbstractDatastoreInputReader(InputReader):
           shard_count,
           namespace,
           query_spec.entity_kind,
+          query_spec.filters,
           app)
       # The nth split of each ns will be assigned to the nth shard.
       # Shuffle so that None are not all by the end.
@@ -546,6 +547,7 @@ class AbstractDatastoreInputReader(InputReader):
                            shard_count,
                            namespace,
                            raw_entity_kind,
+                           filters,
                            app):
     """Split a namespace by scatter index into key_range.KeyRange.
 
@@ -570,7 +572,22 @@ class AbstractDatastoreInputReader(InputReader):
                                keys_only=True)
     ds_query.Order("__scatter__")
     oversampling_factor = 32
-    random_keys = ds_query.Get(shard_count * oversampling_factor)
+    random_keys = None
+    if filters:
+      ds_query_with_filters = copy.copy(ds_query)
+      for (key, op, value) in filters:
+        ds_query_with_filters.update({'%s %s' % (key, op): value})
+        try:
+          random_keys = ds_query_with_filters.Get(shard_count *
+                                                  cls._OVERSAMPLING_FACTOR)
+        except db.NeedIndexError, why:
+          logging.warning('Need to add an index for optimal mapreduce-input'
+                          ' splitting:\n%s' % why)
+          # We'll try again without the filter.  We hope the filter
+          # will filter keys uniformly across the key-name space!
+
+    if not random_keys:
+      random_keys = ds_query.Get(shard_count * cls._OVERSAMPLING_FACTOR)
 
     if not random_keys:
       # There are no entities with scatter property. We have no idea
